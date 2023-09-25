35d53d97c28d83682fdd4d6b87f4d4e009dfabf4
Amrawsome <d21125383@mytudublin.ie>
2023-09-25 20:13:56 +0100

    color alert

--- Week_1_Project/app.js
+++ Week_1_Project/app.js
@@ -5,8 +5,11 @@ const color = ['red','green','purple'];
 
 
 button.addEventListener('click', changeB);
+button.addEventListener('click', changeC_Alert);
 
 function changeB(){
     const colorIndex = parseInt(Math.random()*color.length)
     body.style.backgroundColor=color[colorIndex];
+    alert(color[colorIndex]);
 }
+
