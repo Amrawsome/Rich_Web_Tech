0f32f7210055432f011f7f22c2901186b52e4193
Amrawsome <d21125383@mytudublin.ie>
2023-10-18 15:31:39 +0100

    mobile val done

--- D21125383-2/problem-1/PD.js
+++ D21125383-2/problem-1/PD.js
@@ -6,22 +6,37 @@ const form = document.getElementById("form");
 
 form.addEventListener('submit',(e) => {
     let message = [];
+    let reg_Name = /[^a-zA-Z ]/;
+    let reg_Mobile = /[^0-9]/;
+    //Name Val
+    // if(id_name.value === '' || id_name.value == null){
+    //     message.push("Name is a  required field");
+    // }
 
-     let reg = /[^a-zA-Z ]/;
+    // if(id_name.value.length >=20){
+    //     message.push("Names cannot be more than 20 characters");
+    // }
 
-     if(id_name.value === '' || id_name.value == null){
-        message.push("Name is a  required field");
+    //  if (reg_Name.test(id_name.value)){
+    //     message.push("Please enter letters only")
+    // }
+    //Mobile Val
+    if(mobile_Num.value == null){
+        message.push("Mobile Number is a  required field");
     }
 
-    if(id_name.value.length >=20){
-        message.push("Names cannot be more than 20 characters");
+    if(mobile_Num.value.length != 10){
+        message.push("Mobile Number must be 10 digits long");
     }
 
-     if (reg.test(id_name.value)){
-        message.push("Please enter letters only")
-        
+     if (reg_Mobile.test(mobile_Num.value)){
+        message.push("Please enter numbers only")
     }
+    //Email Val
     
+
+
+
     if(message.length > 0){
         e.preventDefault();
         errorM.innerHTML = message.join(', ');
