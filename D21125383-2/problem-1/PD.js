cafc11948ce6f7ff20882e3b94c6cd925dc1594e
Amrawsome <d21125383@mytudublin.ie>
2023-10-18 17:05:06 +0100

    email val done and all val QA'd

--- D21125383-2/problem-1/PD.js
+++ D21125383-2/problem-1/PD.js
@@ -8,24 +8,26 @@ form.addEventListener('submit',(e) => {
     let message = [];
     let reg_Name = /[^a-zA-Z ]/;
     let reg_Mobile = /[^0-9]/;
+    let reg_Email =  /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+.[a-z]{2,3}$/gm;
+    const email_Check = reg_Email.test(email.value);
     //Name Val
-    // if(id_name.value === '' || id_name.value == null){
-    //     message.push("Name is a  required field");
-    // }
+    if(id_name.value === '' || id_name.value == null){
+        message.push("Name is a  required field");
+    }
 
-    // if(id_name.value.length >=20){
-    //     message.push("Names cannot be more than 20 characters");
-    // }
+    if(id_name.value.length >=20){
+        message.push("Names cannot be more than 20 characters");
+    }
 
-    //  if (reg_Name.test(id_name.value)){
-    //     message.push("Please enter letters only")
-    // }
+     if (reg_Name.test(id_name.value)){
+        message.push("Please enter letters only")
+    }
     //Mobile Val
-    if(mobile_Num.value == null){
+    if(mobile_Num.value == null || mobile_Num.value==""){
         message.push("Mobile Number is a  required field");
     }
 
-    if(mobile_Num.value.length != 10){
+    if( mobile_Num.value !== '' && mobile_Num.value.length != 10){
         message.push("Mobile Number must be 10 digits long");
     }
 
@@ -33,9 +35,19 @@ form.addEventListener('submit',(e) => {
         message.push("Please enter numbers only")
     }
     //Email Val
-    
+    if(email.value === '' || email.value == null){
+        message.push("Email is a  required field");
+    }
+
+    if(email.value.length >40){
+        message.push("Names must be less than 40 characters");
+    }
 
 
+
+     if (email.value !== '' && email_Check==false){
+        message.push("Please follow regular email format")    
+     }
 
     if(message.length > 0){
         e.preventDefault();
