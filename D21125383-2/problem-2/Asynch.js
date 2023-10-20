efc4261464b78cc66209ca157ea3b3700a28cd2d
Amrawsome <d21125383@mytudublin.ie>
2023-10-20 18:19:26 +0100

    word map added and understood

--- D21125383-2/problem-2/Asynch.js
+++ D21125383-2/problem-2/Asynch.js
@@ -14,66 +14,33 @@ const retrievePost = async ()=>{
 // retrievePost();
 
  async function aPG6W () {
-
   const post = await retrievePost();//calling fetch to get data 
   const post_data = post//for refined data
-    .filter(post => post.title.split(' ').length > 6)//splitting 
+    //splitting all titles apart to make them individual and filtering out the ones longer than 6 characters in length
+    .filter(post => post.title.split(' ').length > 6)
+    //creating the new array given the last change to the data by a function
     .map(post => post.title);
-
   console.log(post_data)
-
 }
 
 aPG6W();
 
+async function WFM(){
+  const posts = await retrievePost();//get data
+  const bodyText = posts.map(post => post.body).join(' ');//join all bodies together but seperate by a space
 
-
-const fetchPosts = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  } else {
-    throw new Error('Failed to fetch posts.');
-  }
-};
-
-const listPostTitlesWithMoreThanSixWords = async () => {
-  try {
-    const posts = await fetchPosts();
-    const titlesWithMoreThanSixWords = posts
-      .filter(post => post.title.split(' ').length > 6)
-      .map(post => post.title);
-
-    console.log(titlesWithMoreThanSixWords);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// Call the function to list post titles
-//listPostTitlesWithMoreThanSixWords();
-
-// Function to create a word frequency map for the body contents of the posts
-async function createWordFrequencyMap(){
-try {
-  const posts = await fetchPosts();
-  const bodyText = posts.map(post => post.body).join(' ');
-
-  const words = bodyText.split(/\s+/); // Split by whitespace to get words
+  const words = bodyText.split(/\s+/); //split using whitespace to divide all words
+  //
   const wordFrequencyMap = words.reduce((map, word) => {
     map[word] = (map[word] || 0) + 1;
     return map;
   }, {});
 
   console.log(wordFrequencyMap);
-} catch (error) {
-  console.error(error);
-}
-};
 
-// Call the function to create the word frequency map
-//createWordFrequencyMap();
+}
+
+WFM();
       
 
 
