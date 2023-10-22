6ce0bde549bd8146debb5cfc9a8e77ee7eb0b95b
Amrawsome <d21125383@mytudublin.ie>
2023-10-22 19:30:08 +0100

    User Details Grabbed and displayed

--- D21125383-2/problem-3/Git.js
+++ D21125383-2/problem-3/Git.js
@@ -1,5 +1,5 @@
 const UnameInput = document.getElementById("id_Name");
-const submit = document.getElementById("git_Search");
+const submit = document.getElementById("git_Srch");
 const Avatar = document.getElementById("avatar")
 const Name = document.getElementById("user_Name");
 const Uname = document.getElementById("user_Uname");
@@ -8,13 +8,22 @@ const Loc = document.getElementById("user_Location");
 const NOG = document.getElementById("user_Gists");
 
 
-submit.addEventListener('click', () => {
+submit.addEventListener('click', (e) => {
     const userName = UnameInput.value;
+    e.preventDefault();//stops page refresh on button hit
 
-    fetch('https://api.github.com/users/${userName}')
+    fetch(`https://api.github.com/users/${userName}`)
         .then(response => response.json())
         .then(data => {
             Avatar.src = data.avatar_url;
+            Name.value = data.name;
+            Uname.value = data.login || 'Not Available';
+            Email.value = data.email || 'Not Available';
+            Loc.value = data.location || 'Not Available';
+            NOG.value = data.public_gists || 'Not Available';
+
         })
+        console.log(userName);
+        
 })
 
