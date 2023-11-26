f136bb2456cded66e04c02945ba14fbd80da8427
Amrawsome <d21125383@mytudublin.ie>
2023-11-26 05:09:37 +0000

    timer working

---
+++ D21125383-Lab-4/problem-2/Timer.js
@@ -0,0 +1,39 @@
+const timerContainer = document.getElementById("timer");
+    const inputHours = document.getElementById("inputHours");
+    const inputMinutes = document.getElementById("inputMinutes");
+    const inputSeconds = document.getElementById("inputSeconds");
+    const startButton = document.getElementById("startButton");
+    const tmbox = document.getElementById("tmbox");
+
+    function formatTime(time) {
+      return time < 10 ? `0${time}` : time;
+    }
+
+    function updateTimerDisplay(hours, minutes, seconds) {
+      tmbox.textContent = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
+    }
+
+    let countdownTime = 0; // Initial countdown time
+
+    // Event listener for the "Start Countdown" button
+    startButton.addEventListener("click", () => {
+      // Set the initial countdown time based on user input
+      countdownTime = (parseInt(inputHours.value) || 0) * 3600 +
+                      (parseInt(inputMinutes.value) || 0) * 60 +
+                      (parseInt(inputSeconds.value) || 0);
+
+      // Create observable for seconds
+      rxjs.interval(1000)
+        .pipe(
+          rxjs.operators.takeWhile(() => countdownTime >= 0)
+        )
+        .subscribe(() => {
+          const hours = Math.floor(countdownTime / 3600);
+          const minutes = Math.floor((countdownTime % 3600) / 60);
+          const seconds = countdownTime % 60;
+
+          updateTimerDisplay(hours, minutes, seconds);
+
+          countdownTime -= 1;
+        });
+    });
