13e53f31f9d22a0a13c565522096224f8176060e
Amrawsome <d21125383@mytudublin.ie>
2023-12-11 03:29:53 +0000

    updated css and completed problem 2

--- D21125383-Lab-5/problem-2/src/App.js
+++ D21125383-Lab-5/problem-2/src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import $ from 'jquery';
+const NoteApp = () => {
+  const [notes, setNote] = useState([]);
+  const [fact, setFact] = useState('');
+  //retrieve api information
+  useEffect(() => {
+        const limit = 1;
+        const apiKey = '2GJaeXKPThcslpA5QK9IOA==QHs3jNFeWkHjPJVL';
+    
+        $.ajax({
+          method: 'GET',
+          url: `https://api.api-ninjas.com/v1/facts?limit=${limit}`,
+          headers: { 'X-Api-Key': apiKey },
+          contentType: 'application/json',
+          success: function (result) {
+            const str = JSON.stringify(result);
+            let n = str.slice(10);
+            let z = n.slice(0, -3);
+            setFact(z);
+          },
+          error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+          }
+        });
+      }, []); // Empty dependency array to ensure the effect runs once when the component mounts
 
-function App() {
+  useEffect(() => {
+    // Clear the localStorage 
+    localStorage.removeItem('notes');
+
+    const storedNotes = localStorage.getItem('notes');
+    if (storedNotes) {
+      setNote(JSON.parse(storedNotes));
+    }
+  }, []);
+
+  const updateDB = () => {
+    // Store the notes as a JSON string in localStorage
+    localStorage.setItem('notes', JSON.stringify(notes));
+  };
+  //add notes
+  const addNote = () => {
+    const newNote = {
+      key: Math.random() * 1000000000000000000000000000000000000,
+      backgroundColor: 'white', // default color
+    };
+    setNote(prevNotes => [...prevNotes, newNote]);
+    updateDB();
+  };
+
+  const deleteNote = (key) => {
+    const updatedNotes = notes.filter((note) => note.key !== key);
+    setNote(updatedNotes);
+    updateDB();
+  };
+
+  const noteChange = () => {
+    updateDB();
+  };
+  //changes color based on option/output picked
+  const colorChange = (e, key) => {
+    const output = e.target.value;
+    setNote((prevNotes) => {
+      const updatedNotes = prevNotes.map((note) => {
+        if (note.key === key) {
+          return {
+            ...note,
+            backgroundColor: output,
+          };
+        }
+        return note;
+      });
+      return updatedNotes;
+    });
+    updateDB();
+  };
+  //jsx for react dom elements
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fact"><p>API Fact: {fact}</p>
+    <div className="tainer">
+      <button className="add" onClick={addNote}>
+        Add note
+      </button>
+      <div className="note" onClick={noteChange}>
+        {notes.map((note) => (
+          <div
+            className="n-input"
+            contentEditable="true"
+            suppressContentEditableWarning={true}
+            key={note.key}
+            style={{ backgroundColor: note.backgroundColor }}
+          >
+            <img
+              className="delete"
+              src="icons8-bin-24.png"
+              alt="Delete"
+              onClick={() => deleteNote(note.key)}
+            />
+            <select
+              className="colors"
+              id={`colid-${note.key}`}
+              value={note.backgroundColor}
+              onChange={(e) => colorChange(e, note.key)}
+            >
+              <option value="red">Red</option>
+              <option value="green">Green</option>
+              <option value="blue">Blue</option>
+            </select>
+          </div>
+        ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default NoteApp;
